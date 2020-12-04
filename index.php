0c47056cc8f0f52250466bedef6b6763382de8a9
dipalianandapatil <dipali@wpgenius.in>
2020-12-04 17:51:36 +0530

    Added html for middle content

--- index.php
+++ index.php
@@ -16,7 +16,52 @@
 
   include 'slider.php';
 ?>
-
+<div class="container">
+<div class="align">
+    <div class="row">
+        <div class="text-box col-sm-4 col-md-4">
+            <div class="text1">
+                <h2 class="heading border-bottom" >Director Message</h2>
+                   <!--  <img src="img/slider_bot.png"> -->
+                     <p class=msg-contain>Its’ heartening to see some signal of global economic revival. Though too early to say so, but I am optimistic about the revival of economy earlier than estimated. With our persistent efforts I am happy to see the</p>
+                         <a href="Directormsg.php" class="readmore">Read more...</a>
+                    
+            </div>    
+        </div>
+​
+    <div class="text-box col-sm-4">
+        <h2 class="heading border-bottom">Why us..?</h2>
+            <ul>
+                <li>
+​
+                    <a href="deals.php">New Deals</a>
+                </li>
+                <li>
+                    <a href="#">Service</a>
+                </li>
+                 <li>
+                    <a href="#">Quality</a>
+                </li>
+                 <li>
+                    <a href="#">want help</a>
+                </li>
+            </ul>
+                
+    </div>
+    <div class="text-box col-sm-4 col-md-4">
+        <div class="text3">
+            <h2 class="heading border-bottom">Testimonial</h2>
+           
+                <p class="blackquote_para msg-contain">Human beings, by changing the inner attitudes of their minds, can change the outer aspects of their lives… .
+​
+                </p>
+                <p class=name>– William James</p>
+            
+        </div>        
+        
+    </div>
+</div>
+</div>
 <?php
   include 'footer.php';
 ?>
