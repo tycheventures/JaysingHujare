31584205ed173935a03ee0f870a7d9fb1ec0ac56
dipalianandapatil <dipali@wpgenius.in>
2020-12-04 19:16:55 +0530

    modified both index and css

--- index.php
+++ index.php
@@ -6,19 +6,18 @@
     <div class="row">
         <div class="text-box col-sm-4 col-md-4">
             <div class="text1">
-                <h2 class="heading border-bottom" >Director Message</h2>
+                <h2 class="heading border-bottom">Director Message</h2>
                    <!--  <img src="img/slider_bot.png"> -->
-                     <p class=msg-contain>Its’ heartening to see some signal of global economic revival. Though too early to say so, but I am optimistic about the revival of economy earlier than estimated. With our persistent efforts I am happy to see the</p>
+                     <p class="msg-contain">Its’ heartening to see some signal of global economic revival. Though too early to say so, but I am optimistic about the revival of economy earlier than estimated. With our persistent efforts I am happy to see the</p>
                          <a href="Directormsg.php" class="readmore">Read more...</a>
                     
             </div>    
         </div>
 ​
-		<div class="text-box col-sm-4">
+		<div class="text-box col-sm-4 col-md-4">
 			<h2 class="heading border-bottom">Why us..?</h2>
 				<ul>
 					<li>
-	​
 						<a href="deals.php">New Deals</a>
 					</li>
 					<li>
@@ -37,7 +36,7 @@
 			<div class="text3">
 				<h2 class="heading border-bottom">Testimonial</h2>
 			   
-					<p class="blackquote_para msg-contain">Human beings, by changing the inner attitudes of their minds, can change the outer aspects of their lives… .
+					<p class="blackquote_para msg-contain">" Human beings, by changing the inner attitudes of their minds, can change the outer aspects of their lives… . "
 	​
 					</p>
 					<p class=name>– William James</p>
