a01107620e108f67aae6a1e2397191b1558eaa07
sohamjoshi827 <soham@wpgenius.in>
2020-12-04 19:39:54 +0530

    Slider added to home page.

---
+++ slider.php
@@ -0,0 +1,39 @@
+<?php
+
+            $dir_name = "assets/images/";
+            $images = glob($dir_name."*.jpg");
+            // echo count($images);
+            // print_r($images);
+            // var_dump($images);  
+?>
+
+<div class="container">
+    
+        <div class="swiper-container mb-4 shadow" style="border: 10px solid white;">
+            <div class="swiper-wrapper ">
+<?php
+        
+          foreach($images as $image) 
+                {
+                     
+?> 
+              
+                    <div class="swiper-slide" ><img src="<?php echo $image;?>"></div> 
+               
+<?php            
+                         
+                 }  
+?>
+
+
+        
+            </div>
+
+
+
+    <div class="swiper-pagination"></div>
+
+    <!-- If we need navigation buttons -->
+       
+
+        </div>
